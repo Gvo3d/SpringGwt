503caed2e4c91b52b83586333a4ef3653c98fd0c
Gvo3d <denis.yakimov89@gmail.com>
2017-02-01 17:33:40 +0200

    inserted gwt components and gwt dispatcher -2

--- src/main/java/com/hellogwt/client/HelloGWT.java
+++ src/main/java/com/hellogwt/client/HelloGWT.java
@@ -1,13 +1,21 @@
 package com.hellogwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class HelloGWT implements EntryPoint {
 
     public void onModuleLoad() {
-        Label greetingLabel = new Label("Hello, GWT!");
+        final Label greetingLabel = new Label("Hello, GWT!");
         RootPanel.get().add(greetingLabel);
+
+        greetingLabel.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent clickEvent) {
+                greetingLabel.setText("i was clicked");
+            }
+        });
     }
 }
